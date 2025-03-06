70007b254214f81d5cef60e269ec133c5c677471
lhgrodsky <logangrodsky@gmail.com>
2025-03-05 20:13:26 -0500

    Update ui.ts
    Updated Footer Message.

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Crump Bot`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Logan G. @ UNC`;
+export const FOOTER_MESSAGE: string = `Customized AI by Logan G. @ UNC. Research Assistant, does not provide legal advice.`;
 export const CLEAR_BUTTON_TEXT: string = `REMAND`;
 export const PAGE_TITLE: string = `Crump`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
